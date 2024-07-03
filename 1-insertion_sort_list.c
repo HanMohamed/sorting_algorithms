0ad6a3ba2daa86682bd011fb73f24951a2620ab0
HanMohamed <hmohamed2@seattleu.edu>
2024-07-02 18:47:25 -0700

    sorting_algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,30 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ *
+ * @list: The list to be printed
+*/
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *tmp;
+	int key;
 
+	if (!*list)
+		return;
+
+	tmp = *list;
+	while (tmp->next)
+	{
+		while (tmp && tmp->n > tmp->next->n)
+		{
+			key = tmp->n;
+			*(int *)&tmp->n = tmp->next->n;
+			*(int *)&tmp->next->n = key;
+			tmp = *list;
+			print_list(*list);
+		}
+		tmp = tmp->next;
+	}
 }
